8e38b24b84db8f8e8735eeaa714e3f7adf894ff1
Agatino Pesce <agatino.pesce@agicap.com>
2022-05-10 20:50:45 +0200

    working without saga etc

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -26,17 +23,22 @@ namespace GettingStarted
 
                         // By default, sagas are in-memory, but should be changed to a durable
                         // saga repository.
-                        x.SetInMemorySagaRepositoryProvider();
+//                        x.SetInMemorySagaRepositoryProvider();
 
                         var entryAssembly = Assembly.GetEntryAssembly();
 
                         x.AddConsumers(entryAssembly);
-                        x.AddSagaStateMachines(entryAssembly);
-                        x.AddSagas(entryAssembly);
-                        x.AddActivities(entryAssembly);
+                        // x.AddSagaStateMachines(entryAssembly);
+                        // x.AddSagas(entryAssembly);
+                        // x.AddActivities(entryAssembly);
 
-                        x.UsingInMemory((context, cfg) =>
+                        x.UsingRabbitMq((context,cfg) =>
                         {
+                            cfg.Host("localhost", "/", h => {
+                                h.Username("admin");
+                                h.Password("admin");
+                            });
+
                             cfg.ConfigureEndpoints(context);
                         });
                     });
